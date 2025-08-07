e837b1562079b3b9a7950bf6ae5b1b2bfe6697aa
lisov1y <kokagahuk@gmail.com>
2025-08-07 06:19:36 -0700

    actions and utils improved readability

--- actions.js
+++ actions.js
@@ -1,83 +1,93 @@
 const utils = require("./utils");
 
 const actions = {
-    /* Energy Acquisition */
-    pickupDroppedEnergy: function(creep) {
-        const droppedEnergy = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
-            filter: resource => resource.resourceType === RESOURCE_ENERGY && resource.amount > 0
-        });
-
-        if (droppedEnergy && creep.pickup(droppedEnergy) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(droppedEnergy);
+    /** ========== HELPER METHODS ========== */
+    moveToIfNotInRange(creep, target, color = '#ffffff') {
+        if (creep.moveTo && target) {
+            creep.moveTo(target, {
+                visualizePathStyle: { stroke: color }
+            });
         }
     },
 
-    harvestEnergy: function(creep) {
+    transferIfNotInRange(creep, target, resource = RESOURCE_ENERGY) {
+        if (creep.transfer(target, resource) === ERR_NOT_IN_RANGE) {
+            this.moveToIfNotInRange(creep, target, '#ffffff');
+        }
+    },
+
+    withdrawIfNotInRange(creep, target, resource = RESOURCE_ENERGY) {
+        if (creep.withdraw(target, resource) === ERR_NOT_IN_RANGE) {
+            this.moveToIfNotInRange(creep, target, '#ffaa00');
+        }
+    },
+
+    /** ========== ENERGY ACQUISITION ========== */
+    pickupDroppedEnergy(creep) {
+        const dropped = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
+            filter: r => r.resourceType === RESOURCE_ENERGY && r.amount > 0
+        });
+
+        if (dropped && creep.pickup(dropped) === ERR_NOT_IN_RANGE) {
+            this.moveToIfNotInRange(creep, dropped, '#ffaa00');
+        }
+    },
+
+    harvestEnergy(creep) {
         this.pickupDroppedEnergy(creep);
 
         if (!creep.memory.targetSource) {
-            const sources = creep.room.find(FIND_SOURCES);
-            const closest = creep.pos.findClosestByPath(sources);
-            if (closest) {
-                creep.memory.targetSource = closest.id;
-            }
+            const closest = creep.pos.findClosestByPath(FIND_SOURCES);
+            if (closest) creep.memory.targetSource = closest.id;
         }
 
         const source = Game.getObjectById(creep.memory.targetSource);
         if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(source);
+            this.moveToIfNotInRange(creep, source, '#ffaa00');
         }
     },
 
-    findStoredEnergy: function(creep) {
+    findStoredEnergy(creep) {
         return creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: structure =>
-                (structure.structureType === STRUCTURE_CONTAINER ||
-                 structure.structureType === STRUCTURE_STORAGE) &&
-                structure.store[RESOURCE_ENERGY] > 0
-        }) || 0;
+            filter: s => [STRUCTURE_CONTAINER, STRUCTURE_STORAGE].includes(s.structureType) &&
+                s.store[RESOURCE_ENERGY] > 0
+        });
     },
 
-    findEnergySource: function(creep) {
-        let target = this.findStoredEnergy(creep);
-        if (!target) {
-            target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-        }
-        return target;
+    findEnergySource(creep) {
+        return this.findStoredEnergy(creep) || creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
     },
 
-    transferEnergy: function(creep) {
-        let target = utils.getExtensions(creep)
-            || utils.getSpawns(creep)
-            || (creep.memory.role === "repairer" && utils.getTowers(creep))
-            || utils.getContainers(creep)
-            || utils.getStorages(creep);
+    transferEnergy(creep) {
+        let target =
+            utils.getExtensions(creep) ||
+            utils.getSpawns(creep) ||
+            (creep.memory.role === "repairer" && utils.getTowers(creep)) ||
+            utils.getContainers(creep) ||
+            utils.getStorages(creep);
 
         if (target) {
-            if (creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
-            }
+            this.transferIfNotInRange(creep, target);
         } else {
             this.buildConstruction(creep);
         }
     },
 
-    /* Building */
-    buildConstruction: function(creep) {
-        const target = this.findConstructionSite(creep);
-        if (target && creep.build(target) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(target);
+    /** ========== BUILDING ========== */
+    findConstructionSite(creep) {
+        return creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES) || null;
+    },
+
+    buildConstruction(creep) {
+        const site = this.findConstructionSite(creep);
+        if (site && creep.build(site) === ERR_NOT_IN_RANGE) {
+            this.moveToIfNotInRange(creep, site, '#00ff00');
         }
     },
 
-    findConstructionSite: function(creep) {
-        const sites = creep.room.find(FIND_CONSTRUCTION_SITES);
-        return sites[0] || 0;
-    },
-
-    /* Maintenance */
-    findDamagedStructure: function(creep) {
-        const priorities = [
+    /** ========== REPAIRING ========== */
+    findDamagedStructure(creep) {
+        const PRIORITY = [
             STRUCTURE_SPAWN,
             STRUCTURE_EXTENSION,
             STRUCTURE_TOWER,
@@ -89,24 +99,52 @@ const actions = {
         ];
 
         const damaged = creep.room.find(FIND_STRUCTURES, {
-            filter: s => {
-                if (s.hits >= s.hitsMax) return false;
-                if (s.structureType === STRUCTURE_WALL && s.hits > 250000) return false;
-                return true;
-            }
+            filter: s =>
+                s.hits < s.hitsMax &&
+                !(s.structureType === STRUCTURE_WALL && s.hits > 250000)
         });
 
-        damaged.sort((a, b) => {
-            return priorities.indexOf(a.structureType) - priorities.indexOf(b.structureType);
-        });
+        damaged.sort((a, b) =>
+            PRIORITY.indexOf(a.structureType) - PRIORITY.indexOf(b.structureType)
+        );
 
         return damaged;
     },
 
-    repairDamagedStructures: function(creep) {
+    repairDamagedStructures(creep) {
         const target = this.findDamagedStructure(creep)[0];
         if (target && creep.repair(target) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(target);
+            this.moveToIfNotInRange(creep, target, '#00ffff');
+        }
+    },
+
+    /** ========== HAULER LOGIC ========== */
+    manageHaulerState(creep) {
+        const isEmpty = creep.store[RESOURCE_ENERGY] === 0;
+        const isFull = creep.store.getFreeCapacity() === 0;
+
+        if (creep.memory.hauling && isEmpty) {
+            creep.memory.hauling = false;
+        } else if (!creep.memory.hauling && isFull) {
+            creep.memory.hauling = true;
+        }
+    },
+
+    runHauler(creep) {
+        this.manageHaulerState(creep);
+
+        if (creep.memory.hauling) {
+            const towers = creep.room.find(FIND_STRUCTURES, {
+                filter: s =>
+                    s.structureType === STRUCTURE_TOWER &&
+                    s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY)
+            });
+
+            const target = creep.pos.findClosestByPath(towers);
+            if (target) this.transferIfNotInRange(creep, target);
+        } else {
+            const source = this.findStoredEnergy(creep);
+            if (source) this.withdrawIfNotInRange(creep, source);
         }
     }
 };
--- utils.js
+++ utils.js
@@ -1,54 +1,54 @@
 const utils = {
-    getExtensions: function(creep) {
+    /**
+     * Generic function to get the first structure of a given type that has free capacity for energy.
+     * @param {Creep} creep - The creep requesting the structure.
+     * @param {string} structureType - The type of structure to look for.
+     * @returns {Structure|null} - Closest structure or null.
+     */
+    getStructureWithFreeEnergyCapacity(creep, structureType) {
         const targets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_EXTENSION &&
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
-            }
+            filter: structure =>
+                structure.structureType === structureType &&
+                structure.store &&
+                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
         });
-        return targets ? targets[0] : 0;
+        return targets.length > 0 ? targets[0] : null;
     },
 
-    getSpawns: function(creep) {
-        const targets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_SPAWN &&
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
-            }
-        });
-        return targets ? targets[0] : 0;
+    /**
+     * Get the closest extension that can accept energy.
+     */
+    getExtensions(creep) {
+        return this.getStructureWithFreeEnergyCapacity(creep, STRUCTURE_EXTENSION);
     },
 
-    getTowers: function(creep) {
-        const targets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_TOWER &&
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
-            }
-        });
-        return targets ? targets[0] : 0;
+    /**
+     * Get the closest spawn that can accept energy.
+     */
+    getSpawns(creep) {
+        return this.getStructureWithFreeEnergyCapacity(creep, STRUCTURE_SPAWN);
     },
 
-    getContainers: function(creep) {
-        const targets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_CONTAINER &&
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
-            }
-        });
-        return targets ? targets[0] : 0;
+    /**
+     * Get the closest tower that can accept energy.
+     */
+    getTowers(creep) {
+        return this.getStructureWithFreeEnergyCapacity(creep, STRUCTURE_TOWER);
     },
 
-    getStorages: function(creep) {
-        const targets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_STORAGE &&
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
-            }
-        });
-        return targets ? targets[0] : 0;
+    /**
+     * Get the closest container that can accept energy.
+     */
+    getContainers(creep) {
+        return this.getStructureWithFreeEnergyCapacity(creep, STRUCTURE_CONTAINER);
     },
 
-}
+    /**
+     * Get the closest storage that can accept energy.
+     */
+    getStorages(creep) {
+        return this.getStructureWithFreeEnergyCapacity(creep, STRUCTURE_STORAGE);
+    }
+};
 
 module.exports = utils;
